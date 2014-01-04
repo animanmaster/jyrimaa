68c946af9eaec9a97d95f3ea0f3e23778a22e4b3
Malik Ahmed <animanmaster@gmail.com>
2014-01-03 23:33:02 -0500

    Add doctest for the hash. It even helped find a logic error! Yay testing!

--- piece_data.py
+++ piece_data.py
@@ -3,10 +3,12 @@ class PieceData:
     MAX_RADIUS = 14
 
     def __init__(self):
-        self.stronger_enemies = [0] * MAX_RADIUS
-        self.stronger_friends = [0] * MAX_RADIUS
-        self.weaker_enemies = [0] * MAX_RADIUS
-        self.weaker_friends = [0] * MAX_RADIUS
+        self.stronger_enemies = [0] * self.MAX_RADIUS
+        self.stronger_friends = [0] * self.MAX_RADIUS
+        self.weaker_enemies = [0] * self.MAX_RADIUS
+        self.weaker_friends = [0] * self.MAX_RADIUS
+        self.trap_distances = []
+        self.boundary_distances = []
         self.hashed = None
 
     def add_friend(self, is_weaker, distance):
@@ -22,16 +24,26 @@ class PieceData:
             self.stronger_enemies[distance - 1] += 1
 
     def get_hash(self, radius=None):
+        '''
+        >>> pd.stronger_friends = [0, 0, 1, 0, 0, 1, 1, 2, 0, 0, 1, 5, 0, 0]
+        >>> pd.weaker_friends   = [0, 2, 0, 10, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        >>> pd.stronger_enemies = [2, 0, 3, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        >>> pd.weaker_enemies   = [0, 1, 1, 0, 1, 0, 0, 0, 8, 0, 0, 4, 0, 0]
+        >>> pd.boundary_distances = [3, 3, 4, 4]
+        >>> pd.trap_distances     = [1, 3, 4, 7]
+        >>> pd.get_hash() # 00.00.02.00.0.1|00.02.00.01.0.0|01.00.03.01.2.1|00.10.00.00.2.1|00.01.01.01.0.0|01.00.00.00.0.0|01.00.00.00.0.1|02.00.00.00.0.0|00.00.00.08.0.0|00.00.00.00.0.0|01.00.00.00.0.0|05.00.00.04.0.0|00.00.00.00.0.0|00.00.01.00.0.0
+        '00000200010002000100010003012100100000210001010100010000000001000000010200000000000000080000000000000100000000050000040000000000000000010000'
+        '''
         if not self.hashed:
             zipped = zip(self.stronger_friends, self.weaker_friends, self.stronger_enemies, self.weaker_enemies)
             values = []
-            for r in range(MAX_RADIUS):
+            for r in range(self.MAX_RADIUS):
                 # Note r's range is [0, MAX_RADIUS), so r is actually the actual radius - 1.
                 for radius_value in zipped[r]:
                     values.append('%02d' % radius_value)
                 # Append the number of boundaries at this range.
-                values.append(str(self.boundary_distances.count(r)))
-                values.append(str(self.trap_distances.count(r)))
+                values.append(str(self.boundary_distances.count(r+1)))
+                values.append(str(self.trap_distances.count(r+1)))
             self.hashed = ''.join(values)
         return self.hashed[0:(radius and 10 * radius or None)]
 
@@ -63,3 +75,7 @@ class PieceData:
         </table></html>
         """ % (self.stronger_friends, self.stronger_enemies, self.weaker_friends, self.weaker_enemies,
                self.trap_distances, self.boundary_distances, self.get_hash())
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(extraglobs={'pd': PieceData()})
