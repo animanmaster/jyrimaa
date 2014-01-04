bf9d3720935b262c45164fa368d689db99f6883b
Malik Ahmed <animanmaster@gmail.com>
2014-01-04 01:21:34 -0500

    More detailed comments plus a test and fix for specific radius hash string

--- piece_data.py
+++ piece_data.py
@@ -25,7 +25,17 @@ class PieceData:
 
     def get_hash(self, radius=None):
         '''
-        Hex data hash for this PieceData.
+        Hex data hash for this PieceData. The structure for a given radius will be like so in binary:
+
+          0000     0000    0000      0000     0000         0000
+          \___________/    \____________/     \__/         \__/
+           |        |       |         |        |            |
+        stronger  weaker   stronger  weaker   number      number
+            friends            enemies         of           of
+                                             boundaries   traps
+
+        (total of 3 bytes [6 hex characters] to represent each aspect in the given radius)
+
 
         >>> pd.stronger_friends = [0, 0, 1, 0, 0, 1, 1, 2, 0, 0, 1, 5, 0, 0]
         >>> pd.weaker_friends   = [0, 2, 0, 10, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -35,6 +45,8 @@ class PieceData:
         >>> pd.trap_distances     = [1, 3, 4, 7]
         >>> pd.get_hash() # 00.00.02.00.0.1|00.02.00.01.0.0|01.00.03.01.2.1|00.10.00.00.2.1|00.01.01.01.0.0|01.00.00.00.0.0|01.00.00.00.0.1|02.00.00.00.0.0|00.00.00.08.0.0|00.00.00.00.0.0|01.00.00.00.0.0|05.00.00.04.0.0|00.00.00.00.0.0|00.00.01.00.0.0
         '0020010201001031210a0021011100100000100001200000000800000000100000500400000000001000'
+        >>> pd.get_hash(4)
+        '0020010201001031210a0021'
         '''
         if not self.hashed:
             zipped = zip(self.stronger_friends, self.weaker_friends, self.stronger_enemies, self.weaker_enemies)
@@ -42,12 +54,12 @@ class PieceData:
             for r in range(self.MAX_RADIUS):
                 # Note r's range is [0, MAX_RADIUS), so r is actually the actual radius - 1.
                 for radius_value in zipped[r]:
-                    values.append(str(hex(radius_value)[-1]))
+                    values.append(hex(radius_value)[-1])
                 # Append the number of boundaries at this range.
-                values.append(str(hex(self.boundary_distances.count(r+1)))[-1])
-                values.append(str(hex(self.trap_distances.count(r+1)))[-1])
+                values.append(hex(self.boundary_distances.count(r+1))[-1])
+                values.append(hex(self.trap_distances.count(r+1))[-1])
             self.hashed = ''.join(values)
-        return self.hashed[0:(radius and 10 * radius or None)]
+        return radius and self.hashed[0:(6 * radius)] or self.hashed
 
     def __str__(self):
         return """<html>
