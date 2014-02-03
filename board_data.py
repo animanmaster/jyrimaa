f76348e2abb4c583892bf703b29cc204e394f40d
Malik Ahmed <animanmaster@gmail.com>
2014-02-02 22:38:49 -0500

    Rework the board data hash to put radius data together.

--- board_data.py
+++ board_data.py
@@ -1,16 +1,32 @@
 from piece_data import PieceData
+from move import Move
 
 class BoardData:
 	def __init__(self, board): #, player):
 		self.board = board
 		#self.player = player
+		self._reset_cached_values()
+
+	def _reset_cached_values(self):
 		self._piece_data = None
+		self._chunked = None
 
 	@property
 	def piece_data(self):
 		if self._piece_data is None:
-			self._piece_data = sorted((board.calculate_piece_data(piece) for piece in board.pieces))
+			self._piece_data = sorted((self.board.calculate_piece_data(piece) for piece in self.board.pieces))
 		return self._piece_data
+
+	def chunked_hashes(self):
+		'''
+		Return the full hash chunked by radius.
+
+		>>> bd.chunked_hashes()
+		['002001102001103001', '020100020100020100', '103121103121103121', '0a00210a00210a0021', '011100011100011100', '100000100000100000', '100001100001100001', '200000200000200000', '000800000800000800', '000000000000000000', '100000100000100000', '500400500400500400', '000000000000000000', '001000001000001000']
+		'''
+		if self._chunked is None:
+			self._chunked = [''.join(radius_group) for radius_group in zip(*[pd.chunked_full_hash() for pd in self.piece_data])]
+		return self._chunked
 
 	def hash_value(self, radius=None):
 		'''
@@ -18,25 +34,27 @@ class BoardData:
 		Given a radius, the hash will be a concatenation of all the piece data hashes of the given radius.
 
 		>>> bd.hash_value()
-		'0020010201001031210a00210111001000001000012000000008000000001000005004000000000010001020010201001031210a00210111001000001000012000000008000000001000005004000000000010001030010201001031210a0021011100100000100001200000000800000000100000500400000000001000'
+		'0020011020011030010201000201000201001031211031211031210a00210a00210a0021011100011100011100100000100000100000100001100001100001200000200000200000000800000800000800000000000000000000100000100000100000500400500400500400000000000000000000001000001000001000'
 		>>> bd.hash_value(4)
-		'0020010201001031210a00211020010201001031210a00211030010201001031210a0021'
+		'0020011020011030010201000201000201001031211031211031210a00210a00210a0021'
 		'''
-		return ''.join((pd.get_hash(radius) for pd in self.piece_data))
+		hashes = self.chunked_hashes()
+		return ''.join(radius and hashes[:radius] or hashes)
 
 	def apply_moves(self, moves):
 		'''
 		Apply the given moves to the board.
 		'''
 		for move in moves:
-			self.board.apply_move(Move(move))
-		self._piece_data = None
+			if len(move) > 0:
+				self.board.apply_move(Move(move))
+		self._reset_cached_values()
 
 
 
 if __name__ == "__main__":
     import doctest
-    bd = BoardData(None, None)
+    bd = BoardData(None)
     pd1 = PieceData()
     pd2 = PieceData()
     pd3 = PieceData()
